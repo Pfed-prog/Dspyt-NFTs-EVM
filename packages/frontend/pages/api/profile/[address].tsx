7f18a8da9ced6339f5e58ac31109349f249dcd40
name <fedorovn19@gmail.com>
2024-04-19 15:38:24 +0200

    fixing profile api endpoint

--- packages/frontend/pages/api/profile/[address].tsx
+++ packages/frontend/pages/api/profile/[address].tsx
@@ -1,50 +1,47 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Orbis } from "@orbisclub/orbis-sdk";
 
-let orbis = new Orbis();
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const { address } = req.query;
+  const orbis: IOrbis = new Orbis();
+  const { address } = req.query;
 
-    var username = "";
-    var pfp = "/IconLarge.png";
-    var cover = "/background.png";
-    var description = "";
-    var followers = "";
-    var following = "";
+  const userAddress: string = String(address);
 
-    let { data } = await orbis.getDids(address);
+  var username = "";
+  var pfp = "/IconLarge.png";
+  var cover = "/background.png";
+  var description = "";
+  var followers = "";
+  var following = "";
 
-    if (data[0].address !== undefined) {
-      username = data[0].details.profile?.username;
-      pfp = data[0].details.profile?.pfp;
+  const { data } = await orbis.getDids(userAddress);
 
-      if (
-        typeof data[0].details.profile?.cover === "string" &&
-        data[0].details.profile?.cover !== ""
-      ) {
-        cover = data[0].details.profile?.cover;
-      }
+  if (data[0].address !== undefined) {
+    username = data[0].details.profile?.username;
+    pfp = data[0].details.profile?.pfp;
 
-      description = data[0].details.profile?.description;
-      followers = data[0].details.count_followers;
-      following = data[0].details.count_following;
+    if (
+      typeof data[0].details.profile?.cover === "string" &&
+      data[0].details.profile?.cover !== ""
+    ) {
+      cover = data[0].details.profile?.cover;
     }
 
-    res.status(200).json({
-      address: address,
-      username: username,
-      pfp: pfp,
-      cover: cover,
-      description: description,
-      followers: followers,
-      following: following,
-    });
-  } catch (err) {
-    res.status(500).send({ error: "failed to fetch data" + err });
+    description = data[0].details.profile?.description;
+    followers = data[0].details.count_followers;
+    following = data[0].details.count_following;
   }
+
+  res.status(200).json({
+    address: address,
+    username: username,
+    pfp: pfp,
+    cover: cover,
+    description: description,
+    followers: followers,
+    following: following,
+  });
 }
