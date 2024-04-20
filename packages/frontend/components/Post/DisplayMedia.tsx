755e3acc2a0d631d4cab5e2088acddbfdc8af4b7
name <fedorovn19@gmail.com>
2024-04-20 02:17:17 +0200

    improved image display further; fixing api further

--- packages/frontend/components/Post/DisplayMedia.tsx
+++ packages/frontend/components/Post/DisplayMedia.tsx
@@ -12,9 +12,10 @@ interface IMyProps {
 
 const DisplayMedia: React.FC<IMyProps> = ({ post }) => {
   const xlScreen = useMediaQuery("(min-width: 2000px)");
-  const largeScreen = useMediaQuery("(min-width: 700px)");
-  const height = xlScreen ? 1200 : largeScreen ? 600 : 300;
-  const width = xlScreen ? 800 : largeScreen ? 600 : 200;
+  const largeScreen = useMediaQuery("(min-width: 1200px)");
+  const mediumScreen = useMediaQuery("(min-width: 700px)");
+  const height = xlScreen ? 1200 : largeScreen ? 700 : mediumScreen ? 600 : 300;
+  const width = xlScreen ? 800 : largeScreen ? 600 : mediumScreen ? 400 : 200;
   return (
     <Center>
       {IsNotMp4(post?.image) ? (
@@ -25,8 +26,8 @@ const DisplayMedia: React.FC<IMyProps> = ({ post }) => {
           alt={post.name}
           style={{
             borderRadius: "10px",
-            maxHeight: 500,
-            maxWidth: 500,
+            maxHeight: height,
+            maxWidth: width,
           }}
         />
       ) : (
